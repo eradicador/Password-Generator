cbafe2a411b3bf3a1d4a1361c61fbcf25585e436
eradicador <janisepinto@yahoo.com>
2020-06-12 15:36:54 -0400

    added comments to js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// hooking into the button in the html
 var generateBtn = document.querySelector("#generate");
 // Arrays characters for the password
 var lowerChars = "abcdefghijklmnopqrstuvwxyz";
@@ -26,7 +26,7 @@ function generatePassword() {
   var punctuation = confirm("do you want special characters for your password?");
   console.log('Punctuation ' + punctuation);
 
-  // get  random character from the lower container   // math.floor math.random   (get a number )
+  
   if (lowercase) {
     guaranteedChars += randomize(lowerChars)
     avaiableChars += lowerChars
@@ -61,28 +61,11 @@ function randomize(characters) {
   return characters[randomIndex]
 }
 
-// Write password to the #password input
+// this function calls another function is hooking into 2 elements for the password
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-// Add event listener to generate button
+// when user clicked button it writes the function above
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
